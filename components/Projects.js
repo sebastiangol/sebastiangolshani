97ad77d99ff09fa5727dc36423dd603640102451
sebastiangol <sebastiangolshani@yahoo.co.uk>
2021-11-25 20:12:59 +0000

    Hard-code a replacement project in the showcase

--- components/Projects.js
+++ components/Projects.js
@@ -4,6 +4,7 @@ import holidaysImage from '../public/imagesProjects/holidays.png';
 import socialmediaImage from '../public/imagesProjects/socialmedia.png';
 import moviesImage from '../public/imagesProjects/movies.png';
 import emailserviceImage from '../public/imagesProjects/emailservice.png';
+import sebgramImage from '../public/imagesProjects/sebgram.png';
 
 function Projects() {
   return (
@@ -19,6 +20,14 @@ function Projects() {
           code="https://github.com/sebastiangol/seb-holidays"
         />
         <ProjectsOption
+          title="Sebgram"
+          img={sebgramImage}
+          description="Responsive social media website for creating socialmedia posts. Based on Instagram."
+          tools="HTML, CSS, JavaScript, React, Next.js, TailwindCSS, Recoil, Heroicons, Firebase, NextAuth, Google Auth"
+          live="https://sebgram.vercel.app/"
+          code="https://github.com/sebastiangol/sebgram"
+        />
+        {/* <ProjectsOption
           title="Seb Socialmedia"
           img={socialmediaImage}
           description="Social media website for viewing/making posts.
@@ -26,7 +35,7 @@ function Projects() {
           tools="HTML, CSS, JavaScript, React, Redux, Material-UI, Firebase, Authentication"
           live="https://react-app-socialmedia.web.app/"
           code="https://github.com/sebastiangol/react-app-socialmedia"
-        />
+        /> */}
         <ProjectsOption
           title="Seb Movies"
           img={moviesImage}
